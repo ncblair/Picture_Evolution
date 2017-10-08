ddf62e3b6f3813f5de207d43306c2475c3e41c61
Nathan Blair <nblair@berkeley.edu>
2017-10-07 21:28:27 -0700

    addin

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ def newImgReq():
     ###CALL MAIN.PY ON DEFAULT PARAMS###
     engine()
     return render_template('newImg.html')
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
